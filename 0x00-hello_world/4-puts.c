6e614dece0a0bc5a9c6b7d1e18088424df5a1a98
Eric Afriyie <kae.afriyie@gmail.com>
2022-09-08 18:37:23 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,12 +4,11 @@
  
   * Return: 0 if exited properly, non-zero otherwise
  */
-
 int main(void)
 
 {
-		puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
-			return (0);
+	return (0);
 }
 
