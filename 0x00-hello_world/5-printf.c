ec5727dc14ce69414fe79d8f7a5d37043ab6db09
Eric Afriyie <kae.afriyie@gmail.com>
2022-09-08 17:42:25 -0700

    5. Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,13 +4,11 @@
 
 /**
  *
- *  * main - prints the phrase with proper grammar,
+ * main - prints the phrase with proper grammar,
+ * but the outcome is a piece of art,
+ * Return: 0 if exited properly, non-zero otherwise
  *
- *   * but the outcome is a piece of art,
- *
- *    * Return: 0 if exited properly, non-zero otherwise
- *
- *     */
+ */
 
 int main(void)
 
