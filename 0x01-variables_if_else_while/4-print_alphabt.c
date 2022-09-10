40be41f615052867ebd1432346d40a120cafea08
Eric Afriyie <kae.afriyie@gmail.com>
2022-09-09 23:52:07 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -14,15 +14,14 @@ int main(void)
 
 	while (ch <= 'z')
 	{
-	
 		if (ch != 'e' && ch != 'q')
 
-		{											putchar(ch);
+		{
+			putchar(ch);
 		}
 
 		ch++;
 	}
-	
 	putchar('\n');
 
 	return (0);
