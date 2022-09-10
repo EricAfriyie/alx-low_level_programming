a1387a2f5083205742509d317df84128ca9e47ec
Eric Afriyie <kae.afriyie@gmail.com>
2022-09-09 23:06:24 -0700

    alphabet game

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -10,19 +10,18 @@ int main(void)
 
 {
 
-		char ch;
+	char ch;
 
+	for (ch = 'a'; ch <= 'z'; ch++)
 
-		for (ch = 'a'; ch <= 'z'; ch++)
+		{
 
-			{
+			putchar(ch);
 
-				putchar(ch);
+		}
 
-			}
+	putchar('\n');
 
-		putchar('\n');
-
-		return (0);
+	return (0);
 
 }
