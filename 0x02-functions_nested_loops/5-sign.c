33b4421944dfd30f9eabd4a44347a7f851c1b54b
Eric Afriyie <kae.afriyie@gmail.com>
2022-09-14 00:08:28 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -15,13 +15,12 @@ int print_sign(int n)
 	if (n > 0)
 
 	{
-
 		_putchar('+');
 
 		return (1);
 
 	}
-	
+
 	else if (n == 0)
 
 	{
@@ -29,7 +28,7 @@ int print_sign(int n)
 		_putchar(48);
 		return (0);
 	}
-	
+
 	else if (n < 0)
 	{
 		_putchar('-');
