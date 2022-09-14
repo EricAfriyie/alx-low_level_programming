de5ca2f4d3f1d59ed33434adac9b595c0eeba15d
Eric Afriyie <kae.afriyie@gmail.com>
2022-09-14 11:58:00 -0700

    9-times_table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,7 +9,6 @@ void times_table(void)
 
 		int i, j, k;
 
-		
 		for (i = 0; i < 10; i++)
 
 		{
@@ -19,7 +18,7 @@ void times_table(void)
 				if (j == 0)
 				{
 					_putchar(k + '0');
-				} 
+				}
 				if (k < 10 && j != 0)
 				{
 					_putchar(',');
@@ -27,7 +26,6 @@ void times_table(void)
 					_putchar(' ');
 					_putchar(k + '0');
 				}
-					
 				else if (k >= 10)
 				{
 					_putchar(',');
@@ -35,7 +33,6 @@ void times_table(void)
 					_putchar((k / 10) + '0');
 					_putchar((k % 10) + '0');
 				}
-				
 			}
 			_putchar('\n');
 		}
