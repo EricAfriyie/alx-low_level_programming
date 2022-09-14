b88c875e497b855a6ac93bc7c50033ac88f18eca
Eric Afriyie <kae.afriyie@gmail.com>
2022-09-14 11:21:10 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,7 +13,8 @@ void print_to_98(int n)
 
 {
 	int i, j;
-	if (n <= 98)
+
+	iif (n <= 98)
 	{
 		for (i = n; i <= 98; i++)
 		{
@@ -23,11 +24,11 @@ void print_to_98(int n)
 				printf("%d\n", i);
 		}
 	}
-	else if (n >=98)
+	else if (n >= 98)
 	{
 		for (j = n; j >= 98; j--)
 		{
-			if(j !=98)
+			if(j != 98)
 				printf("%d, ", j);
 			else if (j == 98)
 				printf("%d\n", j);
