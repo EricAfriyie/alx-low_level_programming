369f3427a9ef12894b9872adfdc2fc83b99c5ed1
Eric Afriyie <kae.afriyie@gmail.com>
2022-09-30 11:03:07 -0700

    Neither irony nor sarcasm is argument

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -31,27 +31,20 @@ int _atoi(char *s)
 			digit = s[i] - '0';
 			if (d % 2)
 				digit = -digit;
+
 			n = n * 10 + digit;
 			f = 1;
 
 			if (s[i + 1] < '0' || s[i + 1] > '9')
 				break;
-
 			f = 0;
-
 		}
-
 		i++;
-
 	}
-
-
 
 	if (f == 0)
 		return (0);
-
 	return (n);
-
 }
 
 /**
@@ -67,17 +60,15 @@ int main(int argc, char *argv[])
 
 	if (argc < 3 || argc > 3)
 	{
-
 		printf("Error\n");
 		return (1);
-
 	}
 
 	num1 = _atoi(argv[1]);
 	num2 = _atoi(argv[2]);
+
 	result = num1 * num2;
 
 	printf("%d\n", result);
-
 	return (0);
 }
